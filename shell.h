22302d6176cde107e1c3deaed754e84b076f0dab
goldenjeffempire <jeffemuodafe124@gmail.com>
2023-11-14 21:52:18 -0800

    Command

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+int main(__attribute((unused))int argc, char **argv)
